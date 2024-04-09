0ff242767b555d2904d68ad249f4fe0179fed273
vanessa <bohyunparkv@gmail.com>
2024-04-09 19:09:17 +0900

    Java tutorials: basics

---
+++ java_tutorials/src/org/opentutorials/javatutorials/variables/IntegerDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		int a;
+		a = 1;
+		System.out.println(a+1);
+	}
+
+}
